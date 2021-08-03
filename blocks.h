d150ddc8efa16dfd70a52d3130d8d6d479711f93
Kyle Se <kyle@skynet.localdomain>
2021-08-03 17:04:33 +1000

    commit

--- blocks.h
+++ blocks.h
@@ -1,16 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/        /*Command*/                 /*Update Interval*/     /*Update Signal*/
-  {"",             "sb-music",                0,                      1},
-  {"",             "sb-pacpackages",          0,                      2},
-  {"",             "sb-forecast",             18000,                  3},
-  {"",             "sb-mailbox",              180,                    4},
-  {"",             "sb-microphone",           0,                      5},
-  {"",             "sb-volume",               0,                      6},
-  {"",             "sb-redshiftstatus",       600,                    7},
-  {"",             "sb-brightness",           0,                      8},
-  {"",             "sb-battery",              5,                      9},
-  {"",             "sb-clock",                30,                     10},
+  {"",             "sb-music",                0,                      1       },
+  {"",             "sb-pacpackages",          0,                      2       },
+  {"",             "sb-forecast",             18000,                  3       },
+  {"",             "sb-nettraf",              1,                      11      },
+  {"",             "sb-memory",               10,                     12      },
+  /* {"",             "sb-cpu",                  10,                     13      }, */
+  {"",             "sb-mailbox",              180,                    4       },
+  {"",             "sb-microphone",           0,                      5       },
+  {"",             "sb-volume",               0,                      6       },
+  {"",             "sb-redshiftstatus",       600,                    7       },
+  {"",             "sb-brightness",           0,                      8       },
+  {"",             "sb-battery",              5,                      9       },
+  {"",             "sb-clock",                30,                     10      },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
